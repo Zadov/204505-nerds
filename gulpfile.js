459d7b24d5151e1b37535b36232a2299ef875ab3
Aleksandr <zadov112@ya.ru>
2016-12-05 16:40:42 +0300

    nerds

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+// Объявляем наши плагины
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var watch = require('gulp-watch');
+var prefixer = require('gulp-autoprefixer');
+var csscomb = require('gulp-csscomb');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+// Создаем sass задание
+// gulp.src - путь по которому лежит scss-файл который мы будем компилировать
+// gulp.dest - путь в который мы будем генерить нашу css-ку
+// plumber() - не выбрасывать из компилятора если есть ошибки
+// errLogToConsole: true - выводить номер строки в которой допущена ошибка
+// prefixer - добавляем префиксы для последних 15 версий браузеров
+// cssmin - минимизируют css
+// rename - добавляет суффикс min и записываем файл в папку css
+gulp.task('sass', function () {
+    gulp.src('scss/**.scss')
+    .pipe(plumber())
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(prefixer({browsers:['last 15 versions'], cascade: true }))
+    // в файле настройки csscomb: csscomb.json изменил
+    // "space-before-opening-brace": "\n" на "space-before-opening-brace": "1"
+    // для того, чтобы перед открывающими скобками ставился пробел, а не перенос
+    // на новую строку
+    .pipe(csscomb())
+    .pipe(gulp.dest('css'))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('css'))
+});
+
+//Тут все просто, мы полностью просматривает папку в которой лежит scss-файл который 
+//мы хотим скомпилировать, и при любом изменении файлов в этой папки запускаем задание 
+//sass (gulp.task('sass))
+gulp.task('default', function() {
+    gulp.run('sass');
+    gulp.watch('scss/**', function(event) {
+        gulp.run('sass');
+    });
+});
---
+++ js/script.js
@@ -0,0 +1,13 @@
+// оживляем кнопку со списком сайтов
+var site_list = document.querySelector(".site_list");
+var open_btn = document.querySelector(".open_site_list");
+var line = document.querySelector(".line");
+
+if (open_btn) {
+	open_btn.addEventListener("click", function(event) {
+		event.preventDefault();
+		open_btn.classList.toggle("cross_line");
+		line.classList.toggle("hide_line");
+		site_list.classList.toggle("site_list_show");
+	});
+}
